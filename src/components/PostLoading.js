b5c4cfbbe2add43dd3054d887d7fe551ea57a5da
jbbadano <71237272+jbbadano@users.noreply.github.com>
2022-05-04 16:02:05 -0300

    first commit

---
+++ src/components/PostLoading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function PostLoading(Component) {
+	return function PostLoadingComponent({ isLoading, ...props }) {
+		if (!isLoading) return <Component {...props} />;
+		return (
+			<p style={{ fontSize: '25px' }}>
+				Just one second!
+			</p>
+		);
+	};
+}
+export default PostLoading;
